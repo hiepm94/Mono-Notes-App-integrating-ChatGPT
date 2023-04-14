2782da96676c922bd86a96df92cd0952c076ffd7
patrick-tbwfon <hiepmac2020@gmail.com>
2023-04-14 07:31:51 +0700

    add create & save notes function

---
+++ app/src/main/java/com/example/monoapp/activities/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.monoapp.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.monoapp.R;
+import com.example.monoapp.adapters.NotesAdapter;
+import com.example.monoapp.entities.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener((v) ->{
+            startActivityForResult(
+                    new Intent(getApplicationContext(), CreateNoteActivity.class),
+                    REQUEST_CODE_ADD_NOTE
+            );
+        });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
+        getNotes();
+    }
+
+    private void getNotes() {
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return ;
+            }
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+
+            }
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
+    }
+}
